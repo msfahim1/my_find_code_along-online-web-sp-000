b1eaeb53507be727e95f024b58c86ca82cddd43d
Michael Fahim <michael_fahim@yahoo.com>
2019-12-06 01:44:06 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -2,8 +2,10 @@ require 'pry'
 
 def my_find(collection)
   i = 0
-   while i < collection.length
-      yield(collection[i])
-     i = i + 1
-   end
+  while i < collection.length
+    if yield(collection[i])
+      return collection[i]
+    end
+    i = i + 1
+  end
 end
